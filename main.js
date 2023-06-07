9c0b1f96053b4af22d496e2d559677e73e80b314
Kyle-07 <125091796+Kyle-07@users.noreply.github.com>
2023-06-06 23:10:59 -0600

    Update main.js

--- main.js
+++ main.js
@@ -5,34 +5,72 @@ canvas.width = innerWidth;
 canvas.height = innerHeight - 4;
 
 let player = {
-  x: 5,
-  y: 800,
+  x: canvas.width/2,
+  y: canvas.height/2,
   w: 36,
   h: 36,
   xSpeed: 0,
   ySpeed: 0,
   speed: 5,
 };
-
+let enemy = {
+  x: Math.random() * canvas.width,
+  y: Math.random() *canvas.height,
+  w: 50,
+  h: 50,
+xSpeed : 0,
+ySpeed : 0,
+speed : 2
+}
 requestAnimationFrame(animate);
 
 function animate() {
+
+let dx = player.x - enemy.x
+let dy = player.y - enemy.y
+let distance = Math.sqrt((dx * dx) +(dy * dy))
+
+
   player.x += player.xSpeed;
   player.y += player.ySpeed;
+
+  enemy.xSpeed = (dx / distance) * enemy.speed
+  enemy.ySpeed = (dy / distance) * enemy.speed
+
+  enemy.x += enemy.xSpeed;
+  enemy.y += enemy.ySpeed;
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "black";
   ctx.fillRect(player.x, player.y, player.w, player.h);
 
-  // ctx.fillStyle = "red";
-  // ctx.fillRect(canvas.width - 50, 0, 50, canvas.height);
-  // ctx.fillRect(0, 675, 1050, 100);
+  ctx.fillStyle = "red";
+  ctx.fillRect(canvas.width/2 , 100, 50, 50);
+  
+  function spawnEnemy(){
+
+    ctx.fillStyle = "red"
+    ctx.fillRect(enemy.x, enemy.y, enemy.w, enemy.h)
+    console.log("spawnEnemy")
+  }
+
+
+spawnEnemy()
+
+
+
 
   requestAnimationFrame(animate);
+
+
+  
+
+
 }
 addEventListener("keydown", keydownHandler);
 
 function keydownHandler(event) {
+  
   console.log(event.code);
 
   if (event.code == "KeyD") {
@@ -64,3 +102,5 @@ function keyupHandler(event) {
     player.ySpeed = 0;
   }
 }
+
+
